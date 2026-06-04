e85584d6e1ea38f73e92b114814cc4e60b670cea
notkunz <fkunzy@gmail.com>
2026-06-04 08:05:39 +0100

    more clean fixes

--- app/dashboard/run-tma/page.tsx
+++ app/dashboard/run-tma/page.tsx
@@ -90,7 +90,7 @@ const startPolling = (runId: string) => {
   let pollCount = 0
   const pollInterval = setInterval(async () => {
     pollCount++
-    if (pollCount > 72) { // 6 minutes max
+    if (pollCount > 36) { // 3 minutes max
       clearInterval(pollInterval)
       setError('Taking too long. Please try again.')
       setRunning(false)
